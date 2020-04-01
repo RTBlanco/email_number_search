15c1633741cddd39bd2e139fafd48a92b6c89c87
RTBlanco <ronnytoribio1@hotmail.com>
2020-03-31 23:27:58 -0400

    fixed issue #1

--- Searcher/search.py
+++ Searcher/search.py
@@ -3,15 +3,15 @@ from requests_html import HTMLSession
 #TODO: Rethink of a better wayt to search the phone number !
 #TODO: Also might be a good idea to looking into the email as well 
 
-# phone_regex = re.compile(r'''(
-#     (\d{3}|\(\d{3}\))?      # area code
-#     (\s|-|\.)?              # separator
-#     (\d{3})                 # first three digits 
-#     (\s|-|\.)               # separator
-#     (\d{4})                 # last for digits 
-#     (\s*(ext|x|ext.)\s*(\d{2,5}))?      # extension
-#     )''', re.VERBOSE)
-phone_regex = re.compile(r'(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4})',re.VERBOSE)#|\d{3}[-\.\s]??\d{4})',re.VERBOSE)
+phone_regex = re.compile(r'''(
+    (\d{3}|\(\d{3}\))?      # area code
+    (\s|-|\.)?              # separator
+    (\d{3})                 # first three digits 
+    (\s|-|\.)               # separator
+    (\d{4})                 # last for digits 
+    (\s*(ext|x|ext.)\s*(\d{2,5}))?      # extension
+    )''', re.VERBOSE)
+
 email_regex = re.compile(r'''(
     [a-zA-Z0-9._%+-]+       # username
     @                       # @ symbol
@@ -21,19 +21,36 @@ email_regex = re.compile(r'''(
 
 def scraper(link):
     session = HTMLSession()
-    web_html = session.get(link) #user inputed site 
+    web_html = session.get(link)  
+    # letting the Javascript render first 
     web_html.html.render()
-
-
     web_text = web_html.text
-    matches = []
-    for groups in phone_regex.findall(web_text):
-        # phone_num = '-'.join([groups[1],groups[3],groups[5]])
-        # if groups[8] != '':
-            # phone_num += ' x' + groups[8]
-        matches.append(groups)
-    for groups in email_regex.findall(web_text):
-        matches.append(groups[0])
-    return matches
 
-print(scraper('https://www.indeed.com/jobs?q&l=Nashua%2C%20NH&vjk=bc14adcabbeb16e9'))
+    phone_matches = phone_regex.findall(web_text)
+    #TODO: Try a list comprehension 
+    # removing all the the blank lists
+    new_phone_matches = []
+    for found_phones in phone_matches:
+        for phone in found_phones:
+            if len(phone) < 11:
+                del phone
+            else:
+                new_phone_matches.append(phone)
+  
+    email_matches = email_regex.findall(web_text)
+    new_email_matches = []
+    for found_emails in email_matches:
+        for email in found_emails:
+            if len(email) >= 6:
+                new_email_matches.append(email)
+            else:
+                del email
+
+    return new_phone_matches, new_email_matches
+
+phone, email = scraper('https://www.transparent.com')
+
+for number in phone:
+    print(number)
+for mail in email:
+    print(mail)
